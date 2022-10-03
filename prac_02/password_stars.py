9f9796fdd216b8d46462416a8f3439eab6f58acd
Kae Young <kae.young@my.jcu.edu>
2022-10-03 18:27:10 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,23 @@
-password = input("Password: ")
-maxLength = 15
-minLength = 5
-passwordLength = len(password)
-if minLength < passwordLength < maxLength:
+def main():
+    max_length, min_length, password, password_length = get_password()
+    if min_length < password_length < max_length:
+        print_stars(password)
+    else:
+        print(f"Password must be between {min_length} and {max_length} characters long.")
+
+
+def print_stars(password):
     for i in range(0, len(password), 1):
         print("*", end="")
     print()
-else:
-    print(f"Password must be between {minLength} and {maxLength} characters long.")
+
+
+def get_password():
+    password = input("Password: ")
+    max_length = 15
+    min_length = 5
+    password_length = len(password)
+    return max_length, min_length, password, password_length
+
+
+main()
