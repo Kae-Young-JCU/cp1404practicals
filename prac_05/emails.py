5023c0b9dded1376556cbc22a134956ec352332a
Kae Young (LAPTOP) <kae.young@my.jcu.edu.au>
2022-10-19 23:38:28 +1000

    Complete emails.py exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,16 +3,33 @@
 
 
 def main():
-    email = input("Enter you're email address: ")
+    email = input("Enter your email address: ")
+    accounts = {}
     while email != "":
-        get_name(email)
+        fullname = get_name(email)
+        valid_option = False
+        while not valid_option:
+            option = input(f"Is {fullname} your actual name (Y/N): ")
+            if option == "Y":
+                accounts[email] = fullname
+                valid_option = True
+            elif option == "N":
+                name = input("What is your name: ")
+                accounts[email] = name
+                valid_option = True
+            else:
+                print("Invalid option")
         email = input("Enter you're email address: ")
+    print(accounts)
 
 
 def get_name(email):
     username = email.split("@")[0]
-    fullname = " ".join(username.split(".")[0].title(), username.split("."[1].title()))
-    print(fullname)
+    try:
+        fullname = " ".join([username.split(".")[0].title(), username.split(".")[1].title()])
+    except IndexError:
+        fullname = username.title()
+    return fullname
 
 
 main()
