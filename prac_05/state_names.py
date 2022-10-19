c070527dd77411ee087d3a009fd5afc556b4687b
Kae Young <kae.young@my.jcu.edu>
2022-10-19 14:08:03 +1000

    Complete prac 5 state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,14 +5,25 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ")
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+    # EAFP Approach
+    try:
+        print(state_code, "is", CODE_TO_NAME[state_code.upper()])
+    except KeyError:
+        print("Invalid short state")
+    state_code = input("Enter short state: ")
+
+    """ LBYL Approach
+    if state_code.upper() in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code.upper()])
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ")
+    """
+for state in CODE_TO_NAME:
+    print(f"{state:<3} is {CODE_TO_NAME[state]}")
