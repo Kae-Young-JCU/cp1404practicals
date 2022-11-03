d61a7683ed5624fc82c3330b5111d72c4be90273
Kae Young (LAPTOP) <kae.young@my.jcu.edu.au>
2022-11-03 18:13:24 +1000

    Add ProgrammingLanguage class and add objects

---
+++ prac_06/programming_language.py
@@ -0,0 +1,12 @@
+class ProgrammingLanguage:
+    def __init__(self, language, typing, reflection, year):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
+    def __str__(self):
+        return f"{self.language}, {self.typing}, Reflection={self.reflection}, First appeared in {self.year}"
