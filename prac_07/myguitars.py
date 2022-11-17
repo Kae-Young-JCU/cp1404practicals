0803939afd6015f4959362d184b22b60e027e026
Kae Young (library) <kae.young@my.jcu.edu.au>
2022-11-18 09:10:20 +1000

    Add the ability to except user input for additional guitar entries

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -8,6 +8,16 @@ reader = csv.reader(guitar_file)
 for row in reader:
     guitars.append(guitar.Guitar(row[0], int(row[1]), float(row[2])))
 guitar_file.close()
+
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: "))
+    guitars.append(guitar.Guitar(name, year, cost))
+    print(f"{name} ({year}) : ${cost:,.2f} added.")
+    name = input("Name: ")
+
 guitars.sort()
+
 for guitar in guitars:
     print(guitar)
