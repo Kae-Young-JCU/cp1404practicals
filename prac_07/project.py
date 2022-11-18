6cd4b157e3c3fd2f72ff5cc6693b0eda82d17a5a
Kae Young (library) <kae.young@my.jcu.edu.au>
2022-11-18 13:38:15 +1000

    Add display option functionality

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,3 +1,4 @@
+import datetime
 class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         self.name = name
@@ -8,3 +9,14 @@ class Project:
 
     def __str__(self):
         return f"\n{self.name}\t{self.start_date}\t{self.priority}\t{self.cost_estimate}\t{self.completion_percentage}"
+
+    def __format__(self, format_spec):
+        if not format_spec:
+            return str(self)
+        elif format_spec == "d":
+            date = self.start_date.strftime("%x")
+            return f"{self.name}, Started on {date}, Priority of {str(self.priority)}, Estimate cost of " \
+                   f"${self.cost_estimate:.2f}, {str(self.completion_percentage)}%"
+
+    def __lt__(self, other):
+        return self.priority < other.priority
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,4 +1,5 @@
 import project
+import datetime
 
 MENU = "Select an option:\n" \
        "(L)oad\n" \
@@ -10,15 +11,16 @@ MENU = "Select an option:\n" \
 
 
 def main():
+    """The main code block"""
     # Load default projects file
     projects = []
-    load_project("projects.txt", projects)
+    load_projects("projects.txt", projects)
 
     # Menu block
     print(MENU)
     selection = input(">>> ")
     while selection != "":
-        match selection:
+        match selection.upper():
             case "L":
                 # Load project option
                 load_file = input("File name: ")
@@ -27,7 +29,7 @@ def main():
                 if ".txt" not in load_file:
                     print("Please only load .txt files")
                 else:
-                    load_project(load_file, projects)
+                    load_projects(load_file, projects)
             case "S":
                 save_file = input("File name: ")
                 if "." not in save_file:
@@ -35,8 +37,9 @@ def main():
                 if ".txt" not in save_file:
                     print("Please only load .txt files")
                 else:
-                    load_project(load_file, projects)
+                    load_projects(load_file, projects)
             case "D":
+                display_projects(projects)
                 break
             case "F":
                 break
@@ -51,19 +54,52 @@ def main():
         selection = input(">>> ")
 
 
-def load_project(load_file, projects):
+def str_to_date(str_date):
+    parts = str_date.split("/")
+    return datetime.datetime(int(parts[2]), int(parts[1]), int(parts[0]))
+
+
+def load_projects(load_file, projects):
     projects.clear()
     with open(load_file, 'r') as f:
         f.readline()
         for line in f.readlines():
             parts = line.strip().split('\t')
-            projects.append(project.Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4])))
+            projects.append(project.Project(parts[0], str_to_date(parts[1]), int(parts[2]), float(parts[3]), int(parts[4])))
 
 
-def save_project(save_file, projects):
+def save_projects(save_file, projects):
     with open(save_file, 'w+') as f:
         f.write('\n')
         f.writelines(projects)
+
+
+def display_projects(projects):
+    incomplete_projects = []
+    complete_projects = []
+    for project_ in projects:
+        if project_.completion_percentage == 100:
+            complete_projects.append(project_)
+        else:
+            incomplete_projects.append(project_)
+    incomplete_projects.sort(reverse=True)
+    complete_projects.sort(reverse=True)
+    plural = ''
+    if len(incomplete_projects) > 1:
+        plural = 's'
+    print(f"You have {len(incomplete_projects)} incomplete project{plural}:")
+    counter = 1
+    for project_ in incomplete_projects:
+        print(f"{counter}. {project_:d}")
+        counter += 1
+    plural = ''
+    if len(complete_projects) > 1:
+        plural = 's'
+    print(f"You have {len(complete_projects)} complete project{plural}:")
+    counter = 1
+    for project_ in complete_projects:
+        print(f"{counter}. {project_:d}")
+        counter += 1
 
 
 main()
